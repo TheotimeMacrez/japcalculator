d83be8741d310a8fd22feb9145908e5606a6dad9
Théotime Macrez <theotime.macrez@student-cs.fr>
2020-10-27 10:30:45 +0100

    Create parrot.js

---
+++ functions/parrot.js
@@ -0,0 +1,19 @@
+exports.handler = async function(event, context) {
+
+  const message = requestBody.message;
+  
+  if (message == null) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({message: message}),
+    };
+  }
+  
+  else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({message: "Pas de message"}),
+    };
+  }
+  
+};
