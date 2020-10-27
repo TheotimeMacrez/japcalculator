d1331f7544523ee91fdfa428f3bb0dcc5630aa27
Théotime Macrez <theotime.macrez@student-cs.fr>
2020-10-27 10:56:25 +0100

    Update parrot.js

--- functions/parrot.js
+++ functions/parrot.js
@@ -3,7 +3,7 @@ exports.handler = async function(event, context) {
   const requestBody = event.body;
   const message = requestBody.message;
   
-  if (message == null) {
+  if (message === '') {
     return {
       statusCode: 200,
       body: JSON.stringify({message: 'Pas de message'})
